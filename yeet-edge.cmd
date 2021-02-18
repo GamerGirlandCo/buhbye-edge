8871aec1d6c3faa5ad066a11e51dca720bd28c41
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <37163052+GamerGirlandCo@users.noreply.github.com>
2021-02-17 21:06:05 -0500

    added deletion to the script

--- yeet-edge.cmd
+++ yeet-edge.cmd
@@ -2,5 +2,7 @@
 cd "C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe"
 for /R %%F in (*) do takeown /f "%%F"
 icacls . /grant Users:(OI)(CI)F /T
+cd ..
+rmdir /Q /S "C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe"
 echo all done~!
 pause>nul|set/p =...
